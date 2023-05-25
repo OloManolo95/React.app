55600060bd109eb8887642681862dba7f9804c0e
AleksanderKasprzak <123496024+OloManolo95@users.noreply.github.com>
2023-05-25 14:12:37 +0200

    add ColumnForm component with styles

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,23 @@
 import styles from './List.module.scss'
 import Column from '../Column/Column.js'
+import ColumnForm from '../ColumnForm/ColumnForm';
+import { useState } from 'react';
+import shortid from 'shortid';
+
+//import { useEffect } from 'react';
+
 
 const List = () => {
+
+    const [columns, setColumns] = useState([
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' }
+    ]);
+
+    const addColumn = newColumn => {
+		setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon }]);
+    };
 
     return (
         <div>
@@ -10,12 +26,12 @@ const List = () => {
                <p className={styles.description}>Interesting things I want to check out</p>
             </header>
             <section className={styles.columns}>
-              <Column icon="book" title="Books" />
-              <Column icon="film" title="Movies" />
-              <Column icon="gamepad" title="Games" />
+            {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
             </section>
+            <ColumnForm action={addColumn} />
         </div>
     );
+
 };
 
 export default List;
