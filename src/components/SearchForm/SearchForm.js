224f31212a95addc815684b7f84c61208b386f99
AleksanderKasprzak <123496024+OloManolo95@users.noreply.github.com>
2023-06-05 14:25:54 +0200

    add reducer and dispatch to SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,23 @@
 import styles from './SearchForm.module.scss'
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 const SearchForm = () => {
+
+    const [phrase, setPhrase] = useState('');
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({ type: 'UPDATE_SEARCHSTRING', payload: phrase })
+    };
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput  placeholder="Search..." />
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
+            <TextInput  placeholder="Search..." value={phrase} onChange={e => setPhrase(e.target.value)}/>
             <Button>
                 <span className="fa fa-search" />
             </Button>
