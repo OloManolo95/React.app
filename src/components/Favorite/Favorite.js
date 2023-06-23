846a64aebd046ef7a0bbeff36a08382f566eacc8
AleksanderKasprzak <123496024+OloManolo95@users.noreply.github.com>
2023-06-23 13:40:42 +0200

    add favorite card option

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,15 +1,34 @@
 import styles from './Favorite.module.scss';
 import PageTitle from '../PageTitle/PageTitle';
+import Card from '../Card/Card';
+import { getFavoriteCard } from '../../redux/store';
+import { useSelector } from 'react-redux';
 
 const Favorite = () => {
+
+  const favoriteCards = useSelector(getFavoriteCard);
+
+  if (favoriteCards.length === 0) {
+    return(
+      <section className={styles.favorite}>
+        <PageTitle>Favorite</PageTitle>
+        <p className={styles.noCards}>No favorites yet...</p>
+      </section>
+    )
+  }
   return(
     <section className={styles.favorite}>
       <PageTitle>Favorite</PageTitle>
-      <p>
-        Lorem Ipsum.
-      </p>
+      <div className={styles.column}>
+        <ul className={styles.card}>
+          {favoriteCards.map((card) =>
+          (<Card key={card.id} title={card.title} id={card.id} isFavorite={card.isFavorite} />)
+          )}
+        </ul>
+      </div>
     </section>
   );
 };
+
 
 export default Favorite;
--- src/redux/store.js
+++ src/redux/store.js
@@ -16,6 +16,8 @@ export const getColumnsByList = ({columns}, listId) => columns
 
 export const getAllLists = state => state.lists;
 
+export const getFavoriteCard = state => state.cards.filter((card) => card.isFavorite === true);
+
 // action creators
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 
@@ -23,7 +25,10 @@ export const addCard = payload => ({ type: 'ADD_CARD', payload });
 
 export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload });
 
-export const addList = payload =>({ type: 'ADD_LIST', payload });
+export const addList = payload => ({ type: 'ADD_LIST', payload });
+
+export const toggleFavorite = payload => ({ type: 'TOGGLE_CARD_FAVORITE', payload });
+
 
 const reducer = (state, action) => {
   switch(action.type) {
@@ -35,6 +40,8 @@ const reducer = (state, action) => {
       return { ...state, searchString: action.payload };
     case 'ADD_LIST':
       return { ...state, lists: [...state.lists, { ...action.payload, id: shortid() }]};
+    case 'TOGGLE_CARD_FAVORITE':
+      return { ...state, cards: state.cards.map(card => (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card) };
       default:
       return state;
 
